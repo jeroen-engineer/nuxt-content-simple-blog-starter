f50e589eb98e591cec3b8b08512540caf071952e
jeroen-engineer <jeroentheunissen@protonmail.com>
2022-05-30 16:48:02 +0200

    simple starter blog nuxt content

---
+++ nuxt.config.ts
@@ -0,0 +1,7 @@
+import { defineNuxtConfig } from "nuxt";
+export default defineNuxtConfig({
+  modules: ["@nuxt/content"],
+  content: {
+    // https://content.nuxtjs.org/api/configuration
+  },
+});
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<script setup>
+const { data } = await useAsyncData("blog", () =>
+  queryContent("/").sort({ createdAt: 0 }).find()
+);
+</script>
+<template>
+  <div>
+    <div v-for="blog in data" :key="blog.id">
+      <NuxtLink :to="blog._path">
+        <h1>{{ blog.title }}</h1>
+      </NuxtLink>
+      <h2>{{ blog.description }}</h2>
+    </div>
+  </div>
+</template>
