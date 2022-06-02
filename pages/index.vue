299562cd9d9521dfcfecbc546125ec5d1338c740
jeroen-engineer <jeroentheunissen@protonmail.com>
2022-06-02 22:28:58 +0200

    fixed order by date issue

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,29 @@
 <script setup>
 const { data } = await useAsyncData("blog", () =>
-  queryContent("/").sort({ createdAt: 0 }).find()
+  queryContent("/blog").sort({ createdAt: 0 }).find()
 );
+
+const date = (value) =>
+  new Date(value).toLocaleDateString("en-us", {
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+  });
+
+const blogs = data.value.map((blog) => ({
+  ...blog,
+  createdAt: date(blog.createdAt),
+}));
 </script>
+
 <template>
   <div>
-    <div v-for="blog in data" :key="blog.id">
+    <div v-for="blog in blogs" :key="blog.id">
       <NuxtLink :to="blog._path">
         <h1>{{ blog.title }}</h1>
       </NuxtLink>
       <h2>{{ blog.description }}</h2>
+      <p>{{ blog.createdAt }}</p>
     </div>
   </div>
 </template>
